77344d5eacc0f2bf5aafa72894218fb415c33ad3
Leo Dirac <leo.dirac@gmail.com>
2010-12-23 13:17:38 -0800

    Makes admin more prominent on the URLs.  Removes django branding.

--- urls.py
+++ urls.py
@@ -4,13 +4,5 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    (r'^admin/', include(admin.site.urls)),
-    
-    # Example:
-    # (r'^chacct/', include('chacct.foo.urls')),
-
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
+    (r'^', include(admin.site.urls)),
 )
