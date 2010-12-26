9950a1d50c3267f48379b4062570c2b9117a057d
Leo Dirac <leo.dirac@gmail.com>
2010-12-26 14:24:31 -0800

    Basic register display

--- records/views.py
+++ records/views.py
@@ -1,1 +1,44 @@
-# Create your views here.
+
+from django.shortcuts import render_to_response
+from django.contrib.auth.decorators import login_required
+
+import records.models as m
+
+def _row_from_current_balances(current_balances, date, accounts):
+    
+    balances = []
+    for acct in accounts:
+        val = current_balances[acct]
+        balances.append( round(val,2) )
+    row = {'Date': date, 'Balances': balances }
+    return row
+                
+@login_required
+def show_register(self):
+    register = []
+    current_balances = {}
+    for acct in m.Account.objects.all():
+        current_balances[acct]=0
+    current_balances['Date'] = None
+    last_date = None
+    
+    accounts = m.Account.objects.all()
+    
+    for balance in m.Balance.objects.all().order_by('date'):
+        if balance.date != current_balances['Date']:
+            # Emit a row into register
+            if last_date:
+                row = _row_from_current_balances(current_balances, last_date, accounts)
+                register.append(row)
+            last_date = balance.date
+        current_balances[balance.account] = balance.amount
+        
+    return render_to_response("records/show_register.html", {
+                            'register': register,
+                            'accounts': accounts,
+                    })
+    
+    
+    
+    
+
--- urls.py
+++ urls.py
@@ -4,5 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    (r'^register', 'records.views.show_register'),
     (r'^', include(admin.site.urls)),
 )
